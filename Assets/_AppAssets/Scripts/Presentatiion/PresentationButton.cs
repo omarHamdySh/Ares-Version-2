5aed01b910f3750f1b14f3230243f326357ace5d
Omar <omar.h.shafeek@gmail.com>
2019-07-10 21:23:21 +0200

    Presentation Scene and Scripts

---
+++ Assets/_AppAssets/Scripts/Presentatiion/PresentationButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PresentationButton : MonoBehaviour
+{
+
+    public UnityEvent onSpacebar;
+    public UnityEvent onReturn;
+    public TimlineController timeLineController;
+ 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (onSpacebar != null)
+            {
+                timeLineController.currentTimeline++;
+                onSpacebar.Invoke();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (onReturn != null)
+            {
+                timeLineController.currentTimeline--;
+                onReturn.Invoke();
+            }
+        }
+    }
+}
