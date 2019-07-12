a410dc3a6d94d35cbcf38332fbf39130620aed8f
Omar <omar.h.shafeek@gmail.com>
2019-07-13 01:54:17 +0200

    Select wıth mous edıt

--- Assets/_AppAssets/Scripts/Game Logic/CameraAndControllersComponent/TouchFSMController.cs
+++ Assets/_AppAssets/Scripts/Game Logic/CameraAndControllersComponent/TouchFSMController.cs
@@ -86,7 +86,6 @@ public class TouchFSMController : MonoBehaviour
                     Select(selectedObject);
                 }
             }
-
         }
         if (!isDraggingCharacter)
         {
